bcf34092ea3abaf2c1bccac947116c7c4ea90076
post-plateaux <robert.d.montalvo-west@gmail.com>
2024-07-30 15:34:21 -0400

    Add environment file support to Grafana service and reorder token generation logic in entrypoint script.

--- grafana/entrypoint.sh
+++ grafana/entrypoint.sh
@@ -5,12 +5,12 @@ if [ -f /etc/grafana/.env ]; then
   export $(grep -v '^#' /etc/grafana/.env | xargs)
 fi
 
-# Get InfluxDB organization ID
-INFLUXDB_INIT_ORG_ID=$(curl -s -X GET http://influxdb:8086/api/v2/orgs?org=${INFLUXDB_INIT_ORG} \
-  -H "Authorization: Token ${INFLUXDB_INIT_ADMIN_TOKEN}" | jq -r '.orgs[0].id')
-
 # Generate InfluxDB token if it doesn't exist
 if [ -z "$INFLUXDB_INIT_ADMIN_TOKEN" ]; then
+  # Get InfluxDB organization ID
+  INFLUXDB_INIT_ORG_ID=$(curl -s -X GET http://influxdb:8086/api/v2/orgs?org=${INFLUXDB_INIT_ORG} \
+    -H "Authorization: Token ${INFLUXDB_INIT_ADMIN_TOKEN}" | jq -r '.orgs[0].id')
+
   INFLUXDB_INIT_ADMIN_TOKEN=$(curl -s -X POST http://influxdb:8086/api/v2/authorizations \
     -H "Authorization: Token ${INFLUXDB_INIT_ADMIN_TOKEN}" \
     -H "Content-Type: application/json" \
