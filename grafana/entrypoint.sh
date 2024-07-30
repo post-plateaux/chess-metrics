fff71a02f350461e0a1563f59a4fab85585ffac2
post-plateaux <robert.d.montalvo-west@gmail.com>
2024-07-30 16:00:58 -0400

    Remove InfluxDB admin token configuration from Docker setup.

--- grafana/entrypoint.sh
+++ grafana/entrypoint.sh
@@ -11,37 +11,6 @@ until curl -s http://influxdb:8086/health | grep -q '"status":"pass"'; do
   sleep 5
 done
 
-# Generate InfluxDB token if it doesn't exist
-if [ -z "$INFLUXDB_INIT_ADMIN_TOKEN" ]; then
-  # Get InfluxDB organization ID without using the token
-  INFLUXDB_INIT_ORG_ID=$(curl -s -X GET http://influxdb:8086/api/v2/orgs?org=${INFLUXDB_INIT_ORG} | jq -r '.orgs[0].id')
-
-  # Generate a new token
-  INFLUXDB_INIT_ADMIN_TOKEN=$(curl -s -X POST http://influxdb:8086/api/v2/authorizations \
-    -H "Content-Type: application/json" \
-    -d '{
-      "status": "active",
-      "description": "admin token",
-      "orgID": "'"${INFLUXDB_INIT_ORG_ID}"'",
-      "permissions": [
-        {
-          "action": "read",
-          "resource": {
-            "type": "buckets"
-          }
-        },
-        {
-          "action": "write",
-          "resource": {
-            "type": "buckets"
-          }
-        }
-      ]
-    }' | jq -r '.token')
-
-  echo "INFLUXDB_INIT_ADMIN_TOKEN=${INFLUXDB_INIT_ADMIN_TOKEN}" >> /etc/grafana/.env
-  export INFLUXDB_INIT_ADMIN_TOKEN
-fi
 
 # Start Grafana
 /run.sh
